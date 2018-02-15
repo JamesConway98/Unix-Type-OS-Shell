36460e0530fdefd7b5552c778d63db14bc64575c
Halle Rattray CS2016 <halle.rattray.2016@uni.strath.ac.uk>
2018-02-15 13:08:30 +0000

    Replace main.c

--- main.c
+++ main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h> 
 
 void getInput();
 void shell_loop();
 int find_command(char []);
-void fork_command();
 
 int running = 0;
-char tkarray[12][120];
+char *tkarray[512];
 
 
 void main(){
@@ -25,7 +20,7 @@ void shell_loop(){
     while(running == 0){
     	printf(":) ");
 	getInput();
-	find_command(tkarray[0]);
+	/*find_command(*tkarray);*/
     }   
 
 }
@@ -34,82 +29,49 @@ void shell_loop(){
 void getInput(){
 
 char input[512];
-char * token;
+char *token;
 int nt = 0;
 
-for(int i = 0; i<12; i++){
-	for(int j = 0; j < 120; j++)
-		{
-		tkarray[i][j] = '\0';
+for(int i = 0; i<512; i++){
+		tkarray[i] = '\0';
 		}
+
+	if(fgets(input, 512, stdin) == NULL) {
+		printf("\n");
+		running = 1;
+		return;
 	}
 
-		if(fgets(input, 512, stdin) == NULL){
-		running = 1;
-}
 	input[strlen(input)-1] = '\0';
 
-	printf("splitting string %s into tokens: \n", input);
-	token = strtok (input, " ,.");
+	
+	
+	
+	token = strtok (input, " |><&;\t\n");
+
+	if (token == NULL){
+		return;
+	}
 
 	while (token != NULL){
 		printf("'%s' \n", token);
-		strcpy(tkarray[nt], token);
-		token = strtok (NULL, " ,.");
+		tkarray[nt] = token;
+		
+		token = strtok (NULL, " |><&;\t\n");
 		nt++;
 }
+	tkarray[nt] = NULL;
 
-}
 
-int find_command(char c[120]){
-
-	
-if(strncmp("exit", c, 5) == 0){
+if(strcmp(tkarray[0], "exit") == 0){
+	if(nt == 1){
 		running = 1;
-		return 0;		
+	}else{
+		
+		printf("P error\n");		
 
 	}
-
-else if(strncmp("fork", c, 2) == 0){
-		return 1;		
-
-	}
-else if(strncmp("getpath", c, 7) == 0){
-	
-		return 2;	
-
-	}
-
-else if(strncmp("setpath", c, 7) == 0){
-	
-		return 3;		
-
-	}
-	
-	return 0;
-
-
-
+} 
 }
 
-void fork_command(){
 
-pid_t pid;
-
-pid = fork();
-
-if(pid < 0) {
-	fprintf(stderr, "Fork Failed");
-}
-
-else if (pid == 0)
-	execlp("", , NULL); //this line is incomplete. the first 2 paramaters should be user inputted commands
-
-
-else if(pid > 0){
-	wait(NULL);
-	printf("Child Complete\n");
-
-}
-
-}
